aea472fbfbe09b6455e074af47bb18129e251ed1
Mike Silvis <mikesilvis@gmail.com>
2015-03-17 15:20:26 -0700

    fixing server

--- main.rb
+++ main.rb
@@ -28,6 +28,6 @@ end
 
 get '/*' do |path|
   size = params[:size] ? params[:size] : '70x70'
-  updated_path = QueryBase.decode_path(path.gsub(/.png/, '')).gsub('/images/', '/')
+  updated_path = QueryBase.decode_path(path.gsub(/.png/, '')).gsub('images/', '')
   Dragonfly.app.fetch_url("https://s3.amazonaws.com/jumbotron/#{updated_path}.png").thumb(size).to_response(env)
 end
