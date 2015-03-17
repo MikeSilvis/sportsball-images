1769033cc0b7b5017d783e3edf33fcbb35422d20
Mike Silvis <mikesilvis@gmail.com>
2015-03-17 14:22:36 -0700

    adding the asset hash

--- main.rb
+++ main.rb
@@ -7,7 +7,26 @@ Dragonfly.app.configure do
   plugin :imagemagick
 end
 
+class QueryBase
+  ASSET_STRINGS = [
+    'ASSET_STRING_1',
+    'ASSET_STRING_2'
+  ]
+  ASSET_HASHES = ASSET_STRINGS.map do |asset_string|
+    Digest::MD5.hexdigest(asset_string)
+  end
+
+  def self.decode_path(path)
+    path.tap do |updated_path|
+      ASSET_HASHES.each do |asset_hash|
+        updated_path.gsub!("-#{asset_hash}", '')
+      end
+    end
+  end
+end
+
 get '/*' do |path|
   size = params[:size] ? params[:size] : '70x70'
-  Dragonfly.app.fetch_url("https://s3.amazonaws.com/jumbotron/#{path}.png").thumb(size).to_response(env)
+  updated_path = QueryBase.decode_path(path)
+  Dragonfly.app.fetch_url("https://s3.amazonaws.com/jumbotron/#{updated_path}.png").thumb(size).to_response(env)
 end
