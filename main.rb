9a446bfdf0074ad995f5a4ad0121540e21f205d4
Mike Silvis <mikesilvis@gmail.com>
2015-04-05 23:33:00 -0700

    adding sentry reporting

--- main.rb
+++ main.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require 'raven'
 require 'byebug'
 require 'sinatra/reloader' if development?
 require 'dragonfly'
@@ -8,8 +9,23 @@ Dragonfly.app.configure do
   plugin :imagemagick
 end
 
+Raven.configure do |config|
+  config.dsn = ENV['SENTRY_DSN']
+end
+
 get '/*' do |path|
   size = params[:size] ? params[:size] : '70x70'
   updated_path = Sportsball::Assets.decode_path(path.gsub(/.png/, '')).gsub('images/', '').downcase
-  Dragonfly.app.fetch_url("https://s3.amazonaws.com/jumbotron/#{updated_path}.png").thumb(size).to_response(env)
+  Dragonfly.app.fetch_url("https://s3.amazonaws.com/jumbotron/#{updated_path}.png").thumb(size).to_response(env).tap do |response|
+    if response.first == 500
+      Raven.capture_message "Image Failed - #{updated_path}",
+        logger: 'logger',
+        extra: {
+          path: updated_path
+        },
+        tags: {
+          environment: 'production'
+        }
+    end
+  end
 end
